617ce3e80ee0db34508a56b389f3f245a735ea94
DarrenSmith10 <darrensmith@daksmith.co.uk>
2025-05-06 12:08:43 +0100

    comment out line

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "../client/public")));
 
 // Handle GET request at the root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Add routes
